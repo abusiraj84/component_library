2b4b3301024fd41b67d873779465fe1ddceb6591
Raine Petersen <raineforest01@gmail.com>
2021-10-14 17:34:39 -0600

    Its alive!

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-// vite.config.js
-const path = require('path');
-const { defineConfig } = require('vite');
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import path from 'path';
 
 module.exports = defineConfig({
+  plugins: [vue()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/main.js'),
@@ -10,12 +11,8 @@ module.exports = defineConfig({
       fileName: (format) => `component_library.${format}.js`,
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['vue'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: 'Vue',
         },
