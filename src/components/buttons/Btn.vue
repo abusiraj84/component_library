3d1b443d875e3951d796f506ef38268f5a64f19d
Raine Petersen <raineforest01@gmail.com>
2021-10-19 15:19:33 -0600

    removed custom colors to make repo more like a template.

--- src/components/buttons/Btn.vue
+++ src/components/buttons/Btn.vue
@@ -2,7 +2,7 @@
   <div>
     <button
       @click="$emit('click')"
-      class="text-white bg-solcius-blue py-2 px-4 rounded-full"
+      class="text-white bg-blue-500 py-2 px-4 rounded-full"
     >
       {{ label }}
     </button>
