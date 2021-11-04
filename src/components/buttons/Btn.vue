8a94cd0cc0ac7b841515304fb88aad4f69e28554
Raine Petersen <raineforest01@gmail.com>
2021-11-04 16:25:48 -0600

    Update
    Added dark mode support for storybook

--- src/components/buttons/Btn.vue
+++ src/components/buttons/Btn.vue
@@ -2,7 +2,7 @@
   <div>
     <button
       @click="$emit('click')"
-      class="text-white bg-blue-500 py-2 px-4 rounded-full"
+      class="text-white bg-blue-500 dark:bg-gray-800 py-2 px-4 rounded-full"
     >
       {{ label }}
     </button>
